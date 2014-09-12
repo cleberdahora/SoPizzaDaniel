c040cb8085cb563e153d29484f41a613030f9667
Daniel Silva <daniel.s.silva@live.com>
2014-09-12 01:37:58 -0300

    Add support for sourcemaps, ES6 and ng-annotate

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
-var gulp = require('gulp');
+'use strict';
+
+var gulp       = require('gulp');
+var concat     = require('gulp-concat');
+var traceur    = require('gulp-traceur');
+var sourcemaps = require('gulp-sourcemaps');
+var ngAnnotate = require('gulp-ng-annotate');
 
 gulp.task('default', function() {
   // place code for your default task here
   //
 });
+
+gulp.task('js', function() {
+  gulp.src('public/js/**/*.js')
+    .pipe(sourcemaps.init())
+      .pipe(concat('app.js'))
+      .pipe(traceur({ sourceMaps: true, experimental: true }))
+      .pipe(ngAnnotate())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public/dist'));
+});
+
