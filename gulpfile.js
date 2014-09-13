2a51247f975267fb0ec89a3d759bfae76ee2e22e
Daniel Silva <daniel.s.silva@live.com>
2014-09-13 00:56:47 -0300

    Add UglifyJS and symlink support; Add --production parameter on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 'use strict';
 
+var argv       = require('yargs').argv;
 var gulp       = require('gulp');
+var gulpif     = require('gulp-if');
 var concat     = require('gulp-concat');
 var traceur    = require('gulp-traceur');
 var sourcemaps = require('gulp-sourcemaps');
 var ngAnnotate = require('gulp-ng-annotate');
+var symlink    = require('gulp-symlink');
+var uglify     = require('gulp-uglify');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -12,12 +16,16 @@ gulp.task('default', function() {
 });
 
 gulp.task('js', function() {
+  var production = argv.production;
   gulp.src('public/js/**/*.js')
-    .pipe(sourcemaps.init())
+    .pipe(gulpif(!production, symlink('public/dist/js')))
+    .pipe(gulpif(!production, sourcemaps.init()))
       .pipe(concat('app.js'))
       .pipe(traceur({ sourceMaps: true, experimental: true }))
       .pipe(ngAnnotate())
-    .pipe(sourcemaps.write('.'))
+     .pipe(gulpif(production, uglify({ warnings: true })))
+    .pipe(gulpif(!production, sourcemaps.write('.')))
     .pipe(gulp.dest('public/dist'));
 });
+
 
