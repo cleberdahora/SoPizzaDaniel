f5ece66651ae38d8ef3424b0c1acc4478a04e254
Daniel Silva <daniel.s.silva@live.com>
2014-09-13 22:07:43 -0300

    Add Sass compiler; Rename remaining "public" directory to "app" directory

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,19 @@ var sourcemaps = require('gulp-sourcemaps');
 var ngAnnotate = require('gulp-ng-annotate');
 var symlink    = require('gulp-symlink');
 var uglify     = require('gulp-uglify');
+var sass       = require('gulp-ruby-sass');
 var argv       = require('yargs')
                   .alias('p', 'production')
                   .argv;
 
-gulp.task('default', function() {
-  // place code for your default task here
-  //
-});
+var production = argv.production;
 
+gulp.task('default', ['js']);
+
+// JavaScript build
 gulp.task('js', function() {
-  var production = argv.production;
   gulp.src('app/js/**/*.js')
-    .pipe(gulpif(!production, symlink('public/dist/js')))
+    .pipe(gulpif(!production, symlink('app/dist/js')))
     .pipe(gulpif(!production, sourcemaps.init()))
       .pipe(concat('app.js'))
       .pipe(traceur({ sourceMaps: true, experimental: true }))
@@ -30,4 +30,12 @@ gulp.task('js', function() {
     .pipe(gulp.dest('app/dist'));
 });
 
-
+// Sass build
+gulp.task('sass', function() {
+  gulp.src('app/sass/**/*.scss')
+    .pipe(gulpif(!production, symlink('app/dist/sass')))
+    .pipe(gulpif(!production, sourcemaps.init()))
+      .pipe(sass(!production ? { sourcemap: true } : {}))
+    .pipe(gulpif(!production, sourcemaps.write('.')))
+    .pipe(gulp.dest('app/dist'));
+});
