3da3931cc1cd8365df66e43220e0c380a0dfbf15
Daniel Silva <daniel.s.silva@live.com>
2014-10-16 01:19:02 -0300

    Add route for SPA main html file

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,12 +7,17 @@ var compression = require('compression');
 var bodyParser = require('body-parser');
 var enrouten = require('express-enrouten');
 var morgan = require('morgan');
+var fs = require('fs');
 
 function mapPath(relativePath) {
   return path.join(global.root, relativePath);
 }
 
 function registerMiddlewares(app) {
+  var privateRoutes = ['api', 'resources'];
+  var expression = '^(?!\/?(' + privateRoutes.join('|') + ')(?![^\/])).*';
+  var regex = new RegExp(expression, 'i');
+
   // Middleware pre-configurations
   express.static.mime.define({
     'text/cache-manifest': ['appcache']
@@ -27,6 +32,15 @@ function registerMiddlewares(app) {
   app.use('/api', enrouten({
     directory: mapPath('api')
   }));
+
+  // SPA
+  app.get(regex, function(req, res, next) {
+    if (req.accepts('html')) {
+      res.send(fs.readFileSync(mapPath('app/dist/html/index.html')));
+    } else {
+      next();
+    }
+  });
 }
 
 module.exports = registerMiddlewares;
