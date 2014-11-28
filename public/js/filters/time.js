044ddb23aff2b2c9a68a01f62ca3e1da86144c1e
Daniel Silva <daniel.s.silva@live.com>
2014-11-28 19:52:48 -0200

    Add place informations

---
+++ public/js/filters/time.js
@@ -0,0 +1,54 @@
+(function() {
+  'use strict';
+
+  function timeFilter(lodash, moment, $window) {
+    function padTime(time) {
+      if (String(time).length >= 4) {
+        return String(time);
+      }
+
+      return padTime('0' + time);
+    }
+
+    function formatTime(timeNumber) {
+      let timeStr = padTime(timeNumber);
+
+      return moment.utc(timeStr, 'HHmm')
+        .format('HH:mm');
+    }
+
+    function formatTimes(times, rangeSeparator, timeSeparator) {
+      if (!angular.isArray(times)) {
+        times = [times];
+      }
+
+      return times.map(time => {
+        if (angular.isObject(time)) {
+          let {start, end} = time;
+
+          if (start === end) {
+            return 'O dia inteiro';
+          }
+
+          return formatTime(start) + rangeSeparator + formatTime(end);
+
+        } else {
+          return formatTime(time);
+        }
+      }).join(timeSeparator);
+    }
+
+    function filter(time, rangeSeparator = '~', timeSeparator = ', ') {
+      if (angular.isArray(time) && !time.length) {
+        return;
+      }
+
+      return formatTimes(time, rangeSeparator, timeSeparator);
+    }
+
+    return filter;
+  }
+
+  angular.module('app')
+    .filter('time', timeFilter);
+})();
