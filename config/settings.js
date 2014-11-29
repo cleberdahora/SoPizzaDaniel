9d43c3824461cea2181530e005ad1d8798b96052
Daniel Silva <daniel.s.silva@live.com>
2014-11-29 13:41:58 -0200

    Fix IP

--- config/settings.js
+++ config/settings.js
@@ -3,7 +3,7 @@
 module.exports = function(app, server) {
   // Set application variables
   app.enable('trust proxy'); // Enable reverse proxy support
-  app.set('ip'    , process.env.IP || '127.0.0.1');
+  app.set('ip'    , process.env.IP || '0.0.0.0');
   app.set('port'  , process.env.PORT);
   app.set('db-uri', process.env.DB_URI || process.env.MONGOHQ_URL);
 };
