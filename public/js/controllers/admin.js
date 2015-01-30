561c567b01812467683f97c37884864003a73a0b
Daniel Silva <daniel.s.silva@live.com>
2015-01-30 00:16:30 -0200

    Improve admin page

--- public/js/controllers/admin.js
+++ public/js/controllers/admin.js
@@ -1,26 +1,39 @@
 (function() {
   'use strict';
 
-  function AdminCtrl(Auth) {
+  function AdminCtrl(Auth, Restangular) {
     let self = this;
+
+    function getPlaces() {
+        // TODO: Handle errors here
+        Restangular.all('places')
+          .getList()
+          .then(places => self.places = places);
+    }
 
     function authenticate(email, password) {
       self.authenticating = true;
       Auth.signIn(email, password)
         .then(() => {
           self.authenticating = false;
+          getPlaces();
         })
         .catch(err => {
           if (err === Auth.UNPROCESSABLE_ENTITY) {
-            alert('Please fill the fields correctly');
+            alert('Preencha os campos corretamente.');
           }
 
           if (err === Auth.UNAUTHORIZED) {
-            alert('Wrong user/password!');
+            alert('Usuário/senha inválido(s)!');
           }
         });
     }
 
+    if (Auth.isSignedIn) {
+      getPlaces();
+    }
+
+    self.isSignedIn     = Auth.isSignedIn;
     self.authenticate   = authenticate;
     self.authenticating = false;
   }
